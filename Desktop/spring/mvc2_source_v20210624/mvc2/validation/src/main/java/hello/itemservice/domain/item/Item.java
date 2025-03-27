86590a5d7c19e4933c4f25ad6b6fe2e3bb95fd87
Jundev0427 <jundev0427@gmail.com>
2025-03-27 15:36:13 +0900

    form 검증 로직 분리

--- Desktop/spring/mvc2_source_v20210624/mvc2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ Desktop/spring/mvc2_source_v20210624/mvc2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -13,18 +13,18 @@ import org.hibernate.validator.constraints.ScriptAssert;
 
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class) // 수정 요구사항
+//    @NotNull(groups = UpdateCheck.class)
     private Long id;
 
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+//    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 10_00_000, groups = {SaveCheck.class, UpdateCheck.class})
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1000, max = 10_00_000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
     @Max(value = 9999, groups = {SaveCheck.class})
     private Integer quantity;
 
